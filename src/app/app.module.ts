6e0b3c2de8db2527cfa8019de354710363d8580a
Ernesto Vazquez <otsenrezeuqzav@gmail.com>
2021-11-19 16:28:39 -0600

    primer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 
+// Tarea:
+// Crear un módulo llamado ContadorModule
+// declaraciones, y exportaciones 
+
+import { HeroesModule } from './heroes/heroes.module';
+import { ContadorModule } from './contador/contador.module';
+
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HeroesModule,
+    // importar aquí
+    ContadorModule 
   ],
   providers: [],
   bootstrap: [AppComponent]
